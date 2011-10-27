6bd5e2666cfc679f78112ef1e84e2b0d6d0d8549
tomatointhesand <beanthedemon@gmail.com>
2011-10-27 16:49:17 -0700

    updated setAcls.sh

--- setAcls.sh
+++ setAcls.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+mkdir -p cakephp/app/config
+mkdir -p cakephp/app/tmp/cache
+mkdir -p cakephp/app/tmp/cache/models
+mkdir -p cakephp/app/tmp/cache/persistent
+mkdir -p cakephp/app/tmp/logs
 setfacl -R -m u:apache:rwx cakephp/app/tmp
 setfacl -R -m u:${USER}:rwx cakephp/app/tmp
 setfacl -R -d -m u:apache:rwx cakephp/app/tmp
